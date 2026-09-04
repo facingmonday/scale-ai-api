cc698a5f0518d8122d60b6bc696b1a32ed3499f1
facingmonday <jason.jmp@gmail.com>
2026-09-04 15:08:52 -0500

    adjust debrief summary grid

--- apps/web/src/components/TeacherDebriefCard.tsx
+++ apps/web/src/components/TeacherDebriefCard.tsx
@@ -127,7 +127,7 @@ const TeacherDebriefCard = ({
                   Anonymized patterns and teaching opportunities for each store type.
                 </p>
               </div>
-              <div className="grid gap-3 lg:grid-cols-2">
+              <div className="grid gap-3 lg:grid-cols-3">
                 {debrief?.profileTypeSummaries?.map((profileType) => (
                   <article
                     key={profileType.key || profileType.label}
